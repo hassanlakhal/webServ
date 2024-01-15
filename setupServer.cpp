447bb451d7fe4eecaf8357b4110803016cd15008
Hassan Lakhal <hlakhal-@e1r7p1.1337.ma>
2024-01-15 21:45:40 +0100

    magre

--- setupServer.cpp
+++ setupServer.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 11:36:12 by hlakhal-          #+#    #+#             */
-/*   Updated: 2024/01/15 21:09:49 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2024/01/15 21:44:39 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void setupServer(webServer& data)
 {
-    size_t numbeOfServer = data.getServer().size();
+    size_t numberOfServer = data.getServer().size();
     std::vector<int> fds;
     struct sockaddr_in host_add;
     struct sockaddr_in client_addr;
     socklen_t client_addrlen = sizeof(client_addr);
-    for (size_t i = 0; i < numbeOfServer; i++)
+    for (size_t i = 0; i < numberOfServer; i++)
     {
         int socket_server = socket(AF_INET,SOCK_STREAM,0);
         if (socket_server < 0)
@@ -29,9 +29,9 @@ void setupServer(webServer& data)
         host_add.sin_port = htons(data.getServer().at(i).getListen());
         host_add.sin_addr.s_addr = htonl(data.getServer().at(i).getHost());
         if (bind(socket_server,reinterpret_cast<struct sockaddr*>(&host_add),sizeof(host_add)) < 0)
-            throw std::runtime_error("Error\nbinding the sokect");
+            throw std::runtime_error("Error\nbinding the socket");
         if (listen(socket_server,SOMAXCONN))
-            throw std::runtime_error("Error\nlistening the sokect");
+            throw std::runtime_error("Error\nlistening the socket");
         fds.push_back(socket_server);
     }
     int client_socket = accept(fds[1], (struct sockaddr *)&client_addr, &client_addrlen);
