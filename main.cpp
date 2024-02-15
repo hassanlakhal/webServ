69464882d9fb3d1da75125ca7e787656ec691d1e
elmustapha3 <mustejjamaay2001@gmail.com>
2024-02-15 13:48:26 +0100

    finishing the project

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: eej-jama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 00:20:40 by hlakhal-          #+#    #+#             */
-/*   Updated: 2024/02/13 18:14:45 by eej-jama         ###   ########.fr       */
+/*   Updated: 2024/02/15 13:47:09 by eej-jama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,19 @@ int main(int ac, char *av[])
         {
             std::string input(av[1]);
             loadingData(input);
+        }
+        else if (ac == 1){
+            std::string conf_file = "./configfile.yaml";
+            std::ifstream cfile;
+            cfile.open(conf_file.c_str());
+            if(cfile.is_open()){
+                cfile.close();
+                loadingData(conf_file);
+            }
+            else{
+                cfile.close();
+                throw std::runtime_error("Error in opening confique file");
+            }
         }
     }
     catch(const std::exception& e)
